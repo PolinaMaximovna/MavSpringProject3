43e4605f523528065a7abb6d811479e09413649d
polina <polina@example.ru>
2020-03-05 23:55:28 +0300

    DI with the help setter

--- src/main/java/ru/java/practice/MusicPlayer.java
+++ src/main/java/ru/java/practice/MusicPlayer.java
@@ -3,8 +3,14 @@ package ru.java.practice;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer(){};
+
     //IoC
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music){
         this.music = music;
     }
 
