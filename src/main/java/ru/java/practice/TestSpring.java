f056db0f462e971fdd22c93f6f29db1b5ae70e90
polina <polina@example.ru>
2020-03-06 00:06:34 +0300

    add to class field (name and volume)

--- src/main/java/ru/java/practice/TestSpring.java
+++ src/main/java/ru/java/practice/TestSpring.java
@@ -14,6 +14,8 @@ public class TestSpring {
         musicPlayer.playMusic();
 
        // System.out.println(ClassicalMusic.getName());
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
